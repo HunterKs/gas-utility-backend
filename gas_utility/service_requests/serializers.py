d1b8054ba21bfad111bf12a69a6e96a94abb0eb6
HunterKs <kushagrasinghal678@gmail.com>
2025-03-26 21:36:16 +0530

    views

--- gas_utility/service_requests/serializers.py
+++ gas_utility/service_requests/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import ServiceRequest
 
 class ServiceRequestSerializer(serializers.ModelSerializer):
+    customer = serializers.ReadOnlyField(source='customer.username')  # Show customer username
+
     class Meta:
         model = ServiceRequest
-        fields = '__all__'  # Includes all model fields
+        fields = '__all__'  # Include all model fields
--- gas_utility/service_requests/views.py
+++ gas_utility/service_requests/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import ServiceRequest
+from .serializers import ServiceRequestSerializer
 
-# Create your views here.
+class ServiceRequestViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing service requests.
+    - Customers can submit service requests.
+    - Admins can view, update, or delete requests.
+    """
+    queryset = ServiceRequest.objects.all()
+    serializer_class = ServiceRequestSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Only authenticated users can create/update requests
+
+    def perform_create(self, serializer):
+        """
+        Assign the logged-in user as the customer when creating a request.
+        """
+        serializer.save(customer=self.request.user)
