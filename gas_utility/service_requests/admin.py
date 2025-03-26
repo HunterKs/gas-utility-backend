33a1552bbcd4004927b4381f67c0582d80816c36
HunterKs <kushagrasinghal678@gmail.com>
2025-03-26 21:15:24 +0530

    admin portal

--- gas_utility/service_requests/admin.py
+++ gas_utility/service_requests/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ServiceRequest
 
-# Register your models here.
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'request_type', 'status', 'submitted_at', 'resolved_at')
+    list_filter = ('status', 'submitted_at')
+    search_fields = ('customer__username', 'request_type')
+
