e8321e6f7d0128410ef71beb11d057242774a00b
HunterKs <kushagrasinghal678@gmail.com>
2025-03-27 16:12:14 +0530

    url correction

--- gas_utility/gas_utility/urls.py
+++ gas_utility/gas_utility/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('service_requests.urls')),
+    path('', include('service_requests.urls')),
 ]
